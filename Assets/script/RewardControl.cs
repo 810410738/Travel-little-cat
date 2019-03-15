955d2768085ed82af739e2a19ed05f98ef881c32
milimilkrabbit <764716522@qq.com>
2019-03-15 17:48:44 +0800

    增加奖励模式和惩罚模式

--- Assets/script/RewardControl.cs
+++ Assets/script/RewardControl.cs
@@ -7,10 +7,12 @@ public class RewardControl : MonoBehaviour
     private MainController Main;
     public int reward = 10;//分数
     public float add_time = 5;//加时
+    public new_ground ground;
     // Start is called before the first frame update
     void Start()
     {
         Main = GameObject.FindWithTag("MainCamera").GetComponent<MainController>();
+        ground = GameObject.Find("Ground").GetComponent<new_ground>();
     }
 
     // Update is called once per frame
@@ -23,21 +25,27 @@ public class RewardControl : MonoBehaviour
     {
         if (collision.tag == "player")
        {
+            Main.Goal(reward);//加分或减分
             if (transform.tag == "reward")//吃到普通道具
             {
-                Main.Goal(reward);//加分或减分
                 AudioManager.Instance.PlaySound("eat");//播放声效
-                Destroy(gameObject);//删除自己
             }
             else if(transform.tag == "reward_add_time")//吃到加时道具
             {
-
                 Main.AddTime(add_time);
-                Main.Goal(reward);
                 AudioManager.Instance.PlaySound("eat");
-                Destroy(gameObject);//删除自己
             }
-            
-       }
+            else if(transform.tag == "super_reward")//超级奖励
+            {
+                ground.flag = 1;
+                AudioManager.Instance.PlaySound("eat");
+            }
+            else if(transform.tag == "punish")//惩罚模式
+            {
+                ground.flag = 2;
+                AudioManager.Instance.PlaySound("eat");
+            }
+            Destroy(gameObject);//删除自己        
+        }
     }
 }
--- Assets/script/new_ground.cs
+++ Assets/script/new_ground.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class new_ground : MonoBehaviour
 {
     public GameObject[] grounds;
+    public int flag = 0;//flag为标志位，0代表随机，1代表奖励地形，2代表惩罚地形,默认为随机
     // Start is called before the first frame update
     void Start()
     {
@@ -22,11 +23,22 @@ public class new_ground : MonoBehaviour
         int[] x_random = new int[]{//用一个数组来模拟概率
           0,0,1,2,3,4  
         };
-                  
-        int x = Random.Range(0, x_random.Length);
-        GameObject temp = Instantiate(grounds[ x_random[x] ], transform);
-        temp.transform.localPosition = new Vector3(21.28f, 0.04f, 1);
-
-        
+        if (this.flag == 0)//随机生成
+        {
+            int x = Random.Range(0, x_random.Length-2);
+            Instantiate(grounds[x_random[x]], transform).transform.localPosition = new Vector3(21.28f, 0.04f, 1);
+        }
+        else if (this.flag == 1)//奖励地形
+        {
+            //grouns数组倒数第二个为奖励地形
+            Instantiate(grounds[x_random[x_random.Length-2]], transform).transform.localPosition = new Vector3(21.28f, 0.04f, 1);
+            flag = 0;//恢复为随机生成
+        }
+        else if (this.flag == 2)//惩罚地形
+        {
+            //grouns数组倒数第一个为奖励地形
+            Instantiate(grounds[x_random[x_random.Length - 1]], transform).transform.localPosition = new Vector3(21.28f, 0.04f, 1);
+            flag = 0;//恢复为随机生成
+        }
     }
 }
